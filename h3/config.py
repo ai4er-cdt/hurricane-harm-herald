ae625fbf15597cdbcfe2951de1a8c23c496cf854
Owen <owen.allemang@gmail.com>
2023-01-22 21:48:53 +0000

    renamed and refactor the name of the folder "hurricane_harm_herald" to h3 to make it easier to type

--- h3/config.py
+++ h3/config.py
@@ -4,6 +4,6 @@ import inspect
 
 
 def get_h3_dir():
-	h3_module = importlib.import_module("hurricane_harm_herald")
+	h3_module = importlib.import_module("h3")
 	h3_dir = os.path.dirname(inspect.getabsfile(h3_module))
 	return os.path.abspath(os.path.join(h3_dir, ".."))
