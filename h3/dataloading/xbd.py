57417d499faf2dbaf207a9402f3cbe3e3165e7fe
Owen Allemang <owen.allemang@gmail.com>
2023-01-26 17:32:56 +0000

    [WIP] loading the data from xbd

---
+++ h3/dataloading/xbd.py
@@ -0,0 +1,20 @@
+import os
+from glob import glob
+
+from h3.utils.directories import get_xbd_disaster_dir, get_xbd_dir
+
+
+def pick_disaster_to_dir(disaster: str) -> None:
+	xbd_dir = get_xbd_dir()
+
+
+def check_xbd():
+	pass
+
+
+def get_xbd():
+	pass
+
+
+def unpack_xbd():
+	pass
