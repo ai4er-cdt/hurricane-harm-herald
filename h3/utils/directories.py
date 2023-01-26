0655f78b6e8abfe5a6e0639e444b5e9418bc2af9
Owen Allemang <owen.allemang@gmail.com>
2023-01-26 17:32:14 +0000

    function to get the directories of xbd and xbd_disaster (of a specific disaster)

--- h3/utils/directories.py
+++ h3/utils/directories.py
@@ -6,3 +6,15 @@ from h3.config import get_h3_dir
 
 def get_data_dir() -> str:
 	return guarantee_existence(os.path.join(get_h3_dir(), "data"))
+
+
+def get_xbd_dir() -> str:
+	return guarantee_existence(os.path.join(get_data_dir(), "xBD_data"))
+
+
+def get_xbd_disaster_dir(disaster: str) -> str:
+	return guarantee_existence(os.path.join(get_xbd_dir(), disaster))
+
+
+def get_xbd_hurricane_dir() -> str:
+	return get_xbd_disaster_dir("hurricane")
