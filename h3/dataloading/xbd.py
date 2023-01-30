f7542f0a4669740278e07bce18f3e524e4ec162f
Owen <owen.allemang@gmail.com>
2023-01-30 13:38:13 +0000

    function to sort out a disaster from xbd

--- h3/dataloading/xbd.py
+++ h3/dataloading/xbd.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 import tarfile
+from pathlib import Path
 from glob import glob
 
 from tqdm import tqdm
-from h3.utils.directories import get_xbd_dir
-from h3.utils.file_ops import get_sha1
+from h3.utils.directories import get_xbd_dir, get_xbd_disaster_dir
+from h3.utils.file_ops import get_sha1, guarantee_existence
+from h3.utils.simple_functions import convert_bytes
 
 SHA1 = {
 	"xview2_geotiff.tgz": "6eae3baddf86796c15638682a6432e3e6223cb39",
@@ -18,14 +20,37 @@ SHA1 = {
 }
 
 
-def pick_disaster_to_dir(disaster: str = "hurricane") -> None:
+def sort_disaster_to_dir(disaster: str = "hurricane") -> None:
 	xbd_dir = get_xbd_dir()
+	disaster_dir = get_xbd_disaster_dir(disaster)
+	all_disaster = glob(f"**/{disaster}-*", root_dir=xbd_dir, recursive=True)
+	total_size = 0
+
+	for file in tqdm(all_disaster, desc=f"Sorting files of {disaster}", unit="file"):
+		abs_filepath = os.path.join(xbd_dir, file)
+		path = Path(file)
+		path_part = path.parts
+
+		# change the folder to "disaster" instead of the default "geotiffs"
+		new_path = os.path.join(disaster_dir, *path_part[1:-1])
+		file_name = path_part[-1]
+
+		guarantee_existence(new_path)
+		new_filepath = os.path.join(new_path, file_name)
+		shutil.copyfile(abs_filepath, new_filepath)
+
+		total_size += os.path.getsize(abs_filepath)
+
+	print(f"Copied {convert_bytes(total_size)}")
 
 
-def check_xbd(filename: str, checksum: bool = False) -> None:
+def check_xbd(filename: str, checksum: bool = False) -> bool:
 	filepath = filename
+	if not os.path.exists(filepath):
+		raise IOError(f"{filename} does not exist")
 	if checksum:
 		assert get_sha1(filepath) == SHA1[filename], f"{filename} failed sha1 checksum"
+	return True
 
 
 def combine_xbd(
@@ -79,7 +104,7 @@ def get_xbd():
 
 
 def main():
-	unpack_xbd()
+	sort_disaster_to_dir()
 
 
 if __name__ == '__main__':
