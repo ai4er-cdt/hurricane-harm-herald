95fc2c01e331567ee43b596bbef2a97c1cf06ef7
Owen <owen.allemang@gmail.com>
2023-01-30 13:34:17 +0000

    indication of the directories the functions create

--- h3/utils/directories.py
+++ h3/utils/directories.py
@@ -5,16 +5,20 @@ from h3.config import get_h3_dir
 
 
 def get_data_dir() -> str:
+	# ./data
 	return guarantee_existence(os.path.join(get_h3_dir(), "data"))
 
 
 def get_xbd_dir() -> str:
+	# ./data/xBD_data
 	return guarantee_existence(os.path.join(get_data_dir(), "xBD_data"))
 
 
 def get_xbd_disaster_dir(disaster: str) -> str:
+	# ./data/xBD_data/"disaster"
 	return guarantee_existence(os.path.join(get_xbd_dir(), disaster))
 
 
 def get_xbd_hurricane_dir() -> str:
+	# ./data/xBD_data/hurricane
 	return get_xbd_disaster_dir("hurricane")
