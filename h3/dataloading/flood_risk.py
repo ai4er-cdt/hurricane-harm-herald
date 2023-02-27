fd3121acf8f5d777b38282d1791d60b74e2642c5
Owen <owen.allemang@gmail.com>
2023-02-27 13:36:35 +0000

    [WIP][DEP] started on converting the Flood_Risk_EF notebook to script

--- h3/dataloading/flood_risk.py
+++ h3/dataloading/flood_risk.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+import os
+
 from h3.utils.downloader import downloader
 from h3.utils.directories import get_flood_dir
+from h3.utils.file_ops import unpack_file
 
 
 def _download_flood():
@@ -9,12 +12,14 @@ def _download_flood():
 	downloader(url, target_dir=get_flood_dir())
 
 
-def _unpack_flood():
-	pass
+def _unpack_flood(filename: str = "NFHL_Key_Layers.gdb.zip"):
+	flood_dir = get_flood_dir()
+	filepath = os.path.join(flood_dir, filename)
+	unpack_file(filepath, "r")
 
 
 def main():
-	_download_flood()
+	_unpack_flood()
 
 
 if __name__ == "__main__":
