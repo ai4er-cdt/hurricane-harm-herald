01f957d3d7cd3b8e92f03cf8c6059990f102af86
Owen <owen.allemang@gmail.com>
2023-02-27 13:34:31 +0000

    created a generic function to untar file

--- h3/utils/file_ops.py
+++ h3/utils/file_ops.py
@@ -1,5 +1,9 @@
 import hashlib
 import os
+import tarfile
+import zipfile
+
+from h3 import logger
 
 
 def guarantee_existence(path: str) -> str:
@@ -22,3 +26,21 @@ def get_sha1(filepath: str) -> str:
 				break
 			sha1.update(data)
 	return sha1.hexdigest()
+
+
+def unpack_file(filepath: str, mode: str):
+	"""
+	Unpack a tar file.
+	It is quite slow for big files
+
+	Parameters
+	----------
+	filepath : str,
+		Path of the file to unpack, it will unpack in the folder
+	mode : str,
+		mode to open the zip/tar file with
+	"""
+	# TODO: this is a bit slow, and not verbose
+	logger.info(f"Unpacking {os.path.basename(filepath)}\nThis can take some time")
+	with tarfile.open(filepath, mode) as tar:
+		tar.extractall(path=os.path.dirname(filepath))
