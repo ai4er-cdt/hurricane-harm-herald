9888871fdb0ddef462d0a10c57e870bdd3bc4220
Owen <owen.allemang@gmail.com>
2023-03-15 01:57:35 +0000

    import the cli_parser and added the parser and logger to __all__

--- h3/_config/__init__.py
+++ h3/_config/__init__.py
@@ -3,8 +3,15 @@ from __future__ import annotations
 import logging
 
 from .logger import make_logger
+from .cli_utils import cli_parser
 
 
+__all__ = [
+	"logger",
+	"parser"
+]
+
+parser = cli_parser()
 logger: logging.Logger
 
 logger = make_logger(
