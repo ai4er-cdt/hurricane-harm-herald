7a91937613b89dc0adb8c0d6aab3832e8ea47cfc
Owen <owen.allemang@gmail.com>
2023-03-15 20:46:07 +0000

    commented out test code

--- h3/__main__.py
+++ h3/__main__.py
@@ -5,8 +5,8 @@ from h3.utils.file_ops import check_all_downloads
 def main():
 	check_all_downloads()
 	h3.logger.info("Getting args")
-	args = h3.parser
-	print(args)
+	# args = h3.parser
+	# print(args)
 
 
 if __name__ == "__main__":
