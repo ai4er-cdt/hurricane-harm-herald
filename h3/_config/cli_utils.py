d4d21a73705405895bdadbaefddec31357b0bc2e
Owen <owen.allemang@gmail.com>
2023-03-15 01:57:08 +0000

    added some arguments to the argparse

--- h3/_config/cli_utils.py
+++ h3/_config/cli_utils.py
@@ -4,5 +4,27 @@ import argparse
 def cli_parser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
-
+		"-z",
+		"--zoom",
+		choices=["0.5", "1", "2", "4"],
+		type=str,
+		default="1",
+		help="Zoom level to use (default: %(default)s)",
 	)
+
+	parser.add_argument(
+		"--model",
+		type=str,
+		help="Model to use",
+		choices=["resnet", "satmae", "swin"]
+	)
+
+	parser.add_argument(
+		"-w",
+		"--weather",
+		type=str,
+		choices=["noaa", "ecmwf", "both"]
+	)
+
+	args = parser.parse_args()
+	return args
