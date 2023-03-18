9eb83eb43e6e0501d4916ce877b11473544d55c2
Owen <owen.allemang@gmail.com>
2023-03-18 02:25:32 +0000

    added docs rst, makefile, and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,9 @@
+import sys
+import os
+# import h3
+sys.path.append(os.path.abspath("../.."))
+# sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,31 +15,58 @@
 project = 'Hurricane Harm Herald - H3'
 copyright = '2023, Owen Allemang, Lisanne Blok, Ruari Marshall-Hawkes, Orlando Timmerman, Peisong Zheng'
 author = 'Owen Allemang, Lisanne Blok, Ruari Marshall-Hawkes, Orlando Timmerman, Peisong Zheng'
-release = '0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import sys, os
-sys.path.append(os.path.abspath(".."))
-
 
 extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.viewcode',
 	'sphinx.ext.napoleon',
+	'sphinx.ext.viewcode',
+	# 'sphinx.ext.autosummary',
 	'sphinx.ext.mathjax',
 	'sphinx-mathjax-offline',
 	]
 
-
-
 templates_path = ['_templates']
 exclude_patterns = []
 
+# SPHINX_APIDOC_OPTIONS=members,show-inheritance
+# add_module_names = False
+# autosummary_generate = True
+
+autodoc_mock_imports = [
+	"affine",
+	"cartopy",
+	"cdsapi",
+	"cv2",
+	"geopandas",
+	"geopy",
+	"matplotlib",
+	"numpy",
+	"numba",
+	"pandas",
+	"PIL",
+	"pytorch_lightning",
+	"rasterio",
+	"rich",
+	"richdem",
+	"shapely",
+	"sklearn",
+	"seaborn",
+	"scipy",
+	"torch",
+	"torchvision",
+	"torchmetrics",
+	"tqdm",
+	"xarray"
+	]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. Hurricane Harm Herald - H3 documentation master file, created by
-   sphinx-quickstart on Thu Mar 16 14:39:57 2023.
+   sphinx-quickstart on Fri Mar 17 19:25:54 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -7,10 +7,11 @@ Welcome to Hurricane Harm Herald - H3's documentation!
 ======================================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
    :caption: Contents:
 
-   modules
+   h3
+
 
 Indices and tables
 ==================
