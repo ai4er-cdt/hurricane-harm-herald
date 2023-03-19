9ca433c6f1d252dd492ccbe7a4c767f547d3514e
Owen <owen.allemang@gmail.com>
2023-03-19 20:22:39 +0000

    moved the SHA1 xbd constant to constants and renamed to SHA1_xbd

--- h3/constants.py
+++ h3/constants.py
@@ -60,3 +60,12 @@ RF_BEST_FEATURES_TO_SCALE = [
 	"storm_surge",
 	"elevation", "slope", "aspect", "dis2coast"
 ]
+SHA1_xbd = {
+	"xview2_geotiff.tgz": "6eae3baddf86796c15638682a6432e3e6223cb39",
+	"xview2_geotiff.tgz.part-aa": "881ed94d1060c91e64c8eae438dfce492a21d9a9",
+	"xview2_geotiff.tgz.part-ab": "4064dddc9aa05f786a3a6f70dd4ca86d79dd9e3a",
+	"xview2_geotiff.tgz.part-ac": "0cfdf761e6f77ac5c423d9fb0927c3f8f8ac43da",
+	"xview2_geotiff.tgz.part-ad": "44a39a7c4a80d386fb71ced95caee040126bb405",
+	"xview2_geotiff.tgz.part-ae": "7fb96fac1d009b6a213d4efef2fbf5f1a475a554",
+	"xview2_geotiff.tgz.part-af": "2ccbd04c4b2e27f8d948de734f661ec0c9d81152"
+}
--- h3/dataloading/xbd.py
+++ h3/dataloading/xbd.py
@@ -6,19 +6,10 @@ from glob import glob
 from tqdm import tqdm
 
 from h3 import logger
+from h3.constants import SHA1_xbd
 from h3.utils.directories import get_xbd_dir, get_xbd_disaster_dir
 from h3.utils.file_ops import get_sha1, guarantee_existence, unpack_file
 from h3.utils.simple_functions import convert_bytes
-
-SHA1 = {
-	"xview2_geotiff.tgz": "6eae3baddf86796c15638682a6432e3e6223cb39",
-	"xview2_geotiff.tgz.part-aa": "881ed94d1060c91e64c8eae438dfce492a21d9a9",
-	"xview2_geotiff.tgz.part-ab": "4064dddc9aa05f786a3a6f70dd4ca86d79dd9e3a",
-	"xview2_geotiff.tgz.part-ac": "0cfdf761e6f77ac5c423d9fb0927c3f8f8ac43da",
-	"xview2_geotiff.tgz.part-ad": "44a39a7c4a80d386fb71ced95caee040126bb405",
-	"xview2_geotiff.tgz.part-ae": "7fb96fac1d009b6a213d4efef2fbf5f1a475a554",
-	"xview2_geotiff.tgz.part-af": "2ccbd04c4b2e27f8d948de734f661ec0c9d81152"
-}
 
 
 def sort_disaster_to_dir(disaster: str = "hurricane") -> None:
